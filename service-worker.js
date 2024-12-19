09b274fce202d40be4b960fc54a0c5103c8d6ee5
Fb Tools <rweenmakween@email.com>
2024-12-19 10:16:01 +0530

    PWA!

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'my-pwa-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/images/favicon.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Service Worker: Caching files');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Fetch from cache if offline
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
+    );
+});
+
+// Activate Service Worker
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
